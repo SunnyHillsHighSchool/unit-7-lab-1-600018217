f4fcbb0a291149092baf486fa5c2acc297036355
Johnathan Mitri <600018217@fjuhsd.org>
2021-01-31 17:12:18 +0000

    wrote listtotal.java

--- ListTotal.java
+++ ListTotal.java
@@ -10,6 +10,15 @@ public class ListTotal
 {
  public static int total( List<Integer> ray )
  {
-  return 0;
+  //declare and initialize sum as 0
+  int sum = 0; 
+  //foreach loop that loops through each value in the arrayList
+  for (Integer val: ray)
+  {
+    //add the current value to sum
+    sum+=val;  
+  }
+  //return the sum
+  return sum;
  }
 }
